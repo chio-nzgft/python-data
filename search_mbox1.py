1f0d645ac328a85c3c288d2cf759939285909650
chio-nzgft <chio@nzgft.com>
2017-01-16 14:11:45 +0800

    Create search_mbox1.py

---
+++ search_mbox1.py
@@ -0,0 +1,28 @@
+#開檔
+handle = open("mbox-short.txt")
+#設定 index  也就是x 為字典
+x=dict()
+#一行一行讀檔
+for line in handle:
+#剔除 "From: "開頭的那行
+    if line.startswith("From:"):
+        continue
+#抓出 "From"開頭的那行
+    if line.startswith("From"):
+#切割那行成多個字串
+        y=line.split()
+#將切割出的第"1" 個字串放到 x  (pythen 第一個字串是 "0")
+#如x是新建的就放index 為 0 再加 1
+#如x是不是新建的就將 index 的值再加 1
+        x[y[1]]=x.get(y[1],0)+1
+#宣告None ...將用於判斷是否第一個
+bigC=None
+#將 x index 一個一個讀出成 字串及index
+for w,c in x.items():
+#判斷 是否是第一個(None)  是就將數值放入
+#找出最大的index
+    if bigC is None or c>bigC:
+        bigC=c
+        bigW=w
+#印出最大的index 及其字串
+print bigW,bigC
