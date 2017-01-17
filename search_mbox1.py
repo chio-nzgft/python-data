4d86b0c7263bf55f5ae6fb50718394530b081d9a
chio-nzgft <chio@nzgft.com>
2017-01-17 14:39:55 +0800

    Update search_mbox1.py

--- search_mbox1.py
+++ search_mbox1.py
@@ -1,5 +1,5 @@
 import urllib
-handle = urllib.urlopen('https://raw.githubusercontent.com/paulcarroty/Python-Snippets/master/Coursera%20Programming%20for%20Everybody%20(Python)/mbox-short.txt')
+handle = urllib.urlopen('https://raw.githubusercontent.com/chio-nzgft/python-data/master/mbox-short.txt')
 #開檔
 
 #設定 dict 也就是x 為字典
