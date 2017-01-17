e0c4438b6a6e3c877e7777c09126cd2d1a3b2003
chio-nzgft <chio@nzgft.com>
2017-01-17 14:18:39 +0800

    Update search_mbox3.py

--- search_mbox3.py
+++ search_mbox3.py
@@ -1,14 +1,13 @@
-handle = open("mbox-short.txt")
+import urllib
+handle = urllib.urlopen('https://raw.githubusercontent.com/paulcarroty/Python-Snippets/master/Coursera%20Programming%20for%20Everybody%20(Python)/mbox-short.txt')
 counts=dict()
 lst=list()
 for line in handle:
-    if line.startswith("From:"):
-        continue
-    if line.startswith("From"):
+    if line.startswith("From") and not line.startswith("From:"):
         z=line.split()[-2:-1][0]
         counts[z[0:2]]=counts.get(z[0:2],0)+1
 for k,v in counts.items():
-    lst.append((k,v))
+    lst.append((k,v)) 
 lst.sort()
 for  k,v in lst:
-    print k,v 
+    print (k,v)
